335b961d30f42cc3b10abfbd0771b37cf4741617
akash patel <akash906129@gmail.com>
2026-05-17 13:52:20 +0530

    impl all engine fn

---
+++ src/engine.rs
@@ -0,0 +1,67 @@
+use crate::market::Market;
+use crate::position::Position;
+use crate::position::Side;
+use std::sync::Condvar;
+use std::sync::Mutex;
+
+#[derive(Debug)]
+pub struct RiskEngine {
+    //positions
+    pub positions: Vec<Position>,
+    pub position_count: u32,
+    pub position_cap: u32,
+
+    //market
+    pub markets: Vec<Market>,
+    pub market_count: u32,
+    pub market_cap: u32,
+
+    pub liq_queue: Vec<u32>,
+    pub liq_queue_count: u32,
+    pub liq_queue_cap: u32,
+
+    pub lock: Mutex<()>,
+    pub price_cond: Condvar,
+    pub liq_cond: Condvar,
+}
+
+impl RiskEngine {
+    pub fn new() -> Self {
+        Self {
+            positions: Vec::with_capacity(1000),
+            position_count: 0,
+            position_cap: 1000,
+
+            markets: Vec::with_capacity(64),
+            market_count: 0,
+            market_cap: 64,
+
+            liq_queue: Vec::with_capacity(256),
+            liq_queue_count: 0,
+            liq_queue_cap: 256,
+            lock: Mutex::new(()),
+            price_cond: Condvar::new(),
+            liq_cond: Condvar::new(),
+        }
+    }
+    pub fn engine_add_position(&mut self, position: Position) {
+        let market_index = position.market_index;
+        let side = position.side;
+        let size = position.size;
+
+        self.positions.push(position);
+
+        if market_index >= self.market_count {
+            return;
+        }
+
+        let market = &mut self.markets[market_index as usize];
+        match side {
+            Side::LONG => market.long_open_intrest += size,
+            Side::SHORT => market.short_open_intrest += size,
+        }
+    }
+    pub fn engine_add_market(&mut self, market: Market) {
+        self.markets.push(market);
+    }
+}
--- src/market.rs
+++ src/market.rs
@@ -1,16 +1,16 @@
 #[derive(Debug, Clone)]
 struct MarketConfig {
-    market_index: u32,
-    initial_margin_rate: u64,
-    maintenance_margin_rate: u64,
+    pub market_index: u32,
+    pub initial_margin_rate: u64,
+    pub maintenance_margin_rate: u64,
 }
 
 #[derive(Debug, Clone)]
 pub struct Market {
-    market_price: u64,
-    long_open_intrest: u64,
-    short_open_intrest: u64,
-    funding_rate: u64,
-    last_funding_time: u64,
-    config: MarketConfig,
+    pub market_price: u64,
+    pub long_open_intrest: u64,
+    pub short_open_intrest: u64,
+    pub funding_rate: u64,
+    pub last_funding_time: u64,
+    pub config: MarketConfig,
 }
