bcb3abf35bc96dc2bdbba972ddc94567a5f47cd0
akash patel <akash906129@gmail.com>
2026-05-23 12:58:22 +0530

    removedd position

--- src/feed.rs
+++ src/feed.rs
@@ -1,53 +1,53 @@
-use crate::engine::RiskEngine;
-use futures_util::{SinkExt, StreamExt};
-use std::sync::{Arc, Condvar, Mutex};
-use tokio_tungstenite::{connect_async, tungstenite::Message};
+// use crate::engine::RiskEngine;
+// use futures_util::{SinkExt, StreamExt};
+// use std::sync::{Arc, Condvar, Mutex};
+// use tokio_tungstenite::{connect_async, tungstenite::Message};
 
-pub async fn feed(engine: Arc<(Mutex<RiskEngine>, Condvar, Condvar)>) {
-    let (lock, price_cond, _) = &*engine;
+// pub async fn feed(engine: Arc<(Mutex<RiskEngine>, Condvar, Condvar)>) {
+//     let (lock, price_cond, _) = &*engine;
 
-    let (socket, _) = connect_async("wss://hermes.pyth.network/ws").await.unwrap();
-    let (mut write, mut read) = socket.split();
-    write
-        .send(Message::Text(
-            r#"{
-        "type": "subscribe",
-        "ids": ["0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"]
-    }"#
-            .into(),
-        ))
-        .await
-        .unwrap();
-    while let Some(Ok(msg)) = read.next().await {
-        if let Message::Text(text) = msg {
-            let json: serde_json::Value = serde_json::from_str(&text).unwrap();
-            if json["type"] != "price_update" {
-                continue;
-            }
+//     let (socket, _) = connect_async("wss://hermes.pyth.network/ws").await.unwrap();
+//     let (mut write, mut read) = socket.split();
+//     write
+//         .send(Message::Text(
+//             r#"{
+//         "type": "subscribe",
+//         "ids": ["0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"]
+//     }"#
+//             .into(),
+//         ))
+//         .await
+//         .unwrap();
+//     while let Some(Ok(msg)) = read.next().await {
+//         if let Message::Text(text) = msg {
+//             let json: serde_json::Value = serde_json::from_str(&text).unwrap();
+//             if json["type"] != "price_update" {
+//                 continue;
+//             }
 
-            let raw_price: i64 = json["price_feed"]["price"]["price"]
-                .as_str()
-                .unwrap()
-                .parse()
-                .unwrap();
+//             let raw_price: i64 = json["price_feed"]["price"]["price"]
+//                 .as_str()
+//                 .unwrap()
+//                 .parse()
+//                 .unwrap();
 
-            let expo = json["price_feed"]["price"]["expo"].as_i64().unwrap();
+//             let expo = json["price_feed"]["price"]["expo"].as_i64().unwrap();
 
-            let mut mark_price = raw_price as u64;
-            for _ in 0..(-expo - 4) {
-                mark_price /= 10;
-            }
+//             let mut mark_price = raw_price as u64;
+//             for _ in 0..(-expo - 4) {
+//                 mark_price /= 10;
+//             }
 
-            let mut eng = lock.lock().unwrap();
-            eng.markets[0].market_price = mark_price;
-            drop(eng);
+//             let mut eng = lock.lock().unwrap();
+//             eng.markets[0].market_price = mark_price;
+//             drop(eng);
 
-            price_cond.notify_one();
-            println!(
-                "SOL mark_price: ${}.{:04}",
-                mark_price / 10000,
-                mark_price % 10000
-            );
-        }
-    }
-}
+//             price_cond.notify_one();
+//             println!(
+//                 "SOL mark_price: ${}.{:04}",
+//                 mark_price / 10000,
+//                 mark_price % 10000
+//             );
+//         }
+//     }
+// }
--- src/v1/liquidate/liquidater.rs
+++ src/v1/liquidate/liquidater.rs
@@ -1,31 +1,26 @@
-pub fn is_liquidatable() -> bool {
+use crate::v1::market::market::Market;
+use crate::v1::position::position::Position;
+
+pub fn is_liquidatable(position: Position, market: Market) -> bool {
     // The minimum margin ratio required to keep a position open.
     // If equity falls below this % of position value → liquidation triggered.
-    // e.g. 0.5 = 50% maintenance requirement
+
     let maintenance_margin_rate: f32 = 0.5;
 
     // The price at which the position was originally opened.
     // Used to calculate unrealized PnL and position size.
     let entry_price: f32 = 20.0;
 
-    // Collateral the trader deposited to open the position.
-    // Higher leverage = less initial margin needed for same position size.
-    let initial_margin: f32 = 12.0;
+  
 
-    // Multiplier on your margin to determine position size.
-    // e.g. 2x leverage on $12 margin = $24 position.
-    // Note: your current value (0.5) means 0.5x — less than 1x, which is unusual.
     let leverage: f32 = 0.5;
 
-    // Current market price of the asset (from oracle/price feed).
-    // Used to calculate unrealized PnL in real time.
-    // In production this comes from Pyth/Switchboard, not hardcoded.
     let market_price: f32 = 2.0;
 
-    let funding_accruals; 
+    let funding_accruals;
 
     // Controlled asset/
-    let position_size = (initial_margin * leverage) / entry_price;
+    let position_size = (position.initial_margin * position.leverage) / entry_price;
     // Profit/loss based on current price vs entry
     let unrealized_PnL = (market_price - entry_price) * position_size;
     // Your current equity = what you put in ± PnL
